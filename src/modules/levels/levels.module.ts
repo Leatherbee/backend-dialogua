d69d55332936a253bb26ae9aa9045413bbbd66ee
filzarr <filzaramadhan2003@gmail.com>
2025-09-16 23:13:40 +0700

    feat: api home-level

---
+++ src/modules/levels/levels.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { LevelsService } from './levels.service';
+import { LevelsController } from './levels.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Level } from './entities/level.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Level]),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads/levels/banners',
+        filename: (req, file, cb) => {
+          const fileExtName = extname(file.originalname);
+          const randomName = uuidv4();
+          return cb(null, `${randomName}${fileExtName}`);
+        },
+      }),
+      limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+      },
+    }),
+  ],
+  controllers: [LevelsController],
+  providers: [LevelsService],
+  exports: [LevelsService],
+})
+export class LevelsModule { }
